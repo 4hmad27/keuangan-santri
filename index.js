b606b6f51fb89854e48e66737725b916e0ab658c
Ahmad Afanshobari <arte.ra272024@gmail.com>
2026-01-07 12:59:23 +0700

    tambah fitur filter bulan lalu

--- index.js
+++ index.js
@@ -158,6 +158,31 @@ app.get("/api/transactions", authMiddleware, async (c) => {
   }
 });
 
+app.get("/api/transactions/all", authMiddleware, async(c) => {
+
+  const user = c.get("user");
+  const userTransactions = await db.query.transactions.findMany({
+    where: eq(transactions.userId, user.id),
+    orderBy: desc(transactions.transactionDate),
+  });
+
+  const totalIncome = userTransactions
+    .filter((t) => t.status === "income")
+    .reduce((sum, t) => sum + Number(t.nominal), 0);
+
+  const totalOutcome = userTransactions
+    .filter((t) => t.status === "outcome")
+    .reduce((sum, t) => sum + Number(t.nominal), 0);
+
+  const balance = totalIncome - totalOutcome;
+
+  return c.json({
+    success: true,
+    data: userTransactions,
+    summary: { totalIncome, totalOutcome, balance },
+  });
+});
+
 app.use("/*", serveStatic({ root: "./public" }));
 
 // app.get('/favicon.ico', (c) => {
